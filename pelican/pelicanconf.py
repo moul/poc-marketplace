c76c8e189acc03fdecc7f0ffec5e03fc11245b13
Manfred Touron <m@42.am>
2015-02-10 16:10:07 +0100

    Enabled pagination

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -31,7 +31,7 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 12
 
 THEME = 'theme'
 
